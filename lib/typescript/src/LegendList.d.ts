5def3a43570edb662151114be778cac1ceef1394
TowhidKashem <towhid.kashem@bit.ly>
2025-07-25 02:41:24 -0400

    build: include compiled output for GitHub installation

---
+++ lib/typescript/src/LegendList.d.ts
@@ -0,0 +1,9 @@
+import type { LegendListProps, LegendListRef } from '@legendapp/list';
+import React from 'react';
+/**
+ * Use like a regular LegendList.
+ */
+export declare const LegendList: <T>(p: LegendListProps<T> & {
+    ref?: React.Ref<LegendListRef>;
+}) => React.ReactElement;
+//# sourceMappingURL=LegendList.d.ts.map
